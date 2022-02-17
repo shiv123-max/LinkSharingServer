0b073ba224f74e14960f9ca4b6caf25ac77af38a
shiv123-max <shivanshusid123@gmail.com>
2022-02-17 22:49:11 +0530

    port updated for deployment

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", dataRoute);
 
-app.listen(9000, () => {
+app.listen(process.env.PORT, () => {
   console.log("backend server is running!");
 });
